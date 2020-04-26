7a85a835dd5d45a9329704447f4dcb4d88597899
YoliaEspinoza <yolia1298@gmail.com>
2020-04-25 19:00:32 -0700

    f

--- sketch.js
+++ sketch.js
@@ -1,7 +1,38 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  background(143, 169, 157);
+
+  strokeWeight(8);
+  // stroke('rgba(245, 183, 183, 1)');
+  // fill(213, 167, 239);
+
+
 }
 
 function draw() {
-  background(220);
+
+  if (mouseIsPressed) {
+    stroke('rgba(245, 183, 183, 1)');
+    stroke(map(mouseX, 0, 1000, 0, 255, true))
+    line(width - mouseX, height - mouseY, width - pmouseX, height - pmouseY);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+
+
+  }
 }
+
+function keyTyped(){
+  // console.log(`key ${key} is being typed`)
+  console.log("key" + key + "is being typed")
+  if(key === 's'){
+    //save this image
+    saveCanvas('fileName', 'png')
+  }
+    else if (key === 'c'){
+  		clear();
+      background(143, 169, 157);
+
+  }
+  return false;
+}
+background(143, 169, 157);
